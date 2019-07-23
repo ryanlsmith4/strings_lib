1073fcb08e618cd63dd2750d1ad4438cb1d6c3d9
Ryan Smith <ryan.l.smith4@gmail.com>
2019-07-23 14:34:18 -0700

    [ADD] npm package works

--- rollup.config.js
+++ rollup.config.js
@@ -3,10 +3,11 @@ import { terser } from 'rollup-plugin-terser';
 export default [
     {
         input: 'src/index.js',
+        plugins: [terser()],
         output: {
             file: 'umd/index.js',
             format: 'umd',
-            name: '@ryanlsmith-stringsLib',
+            name: 'stringsLib',
             esModule: false
         }
     },
